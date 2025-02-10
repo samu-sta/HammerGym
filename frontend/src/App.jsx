58af2aa88eeb57d414b0f81ae6b01045346a5dd8
Samuel <samuelfelipelorente@gmail.com>
2025-02-10 20:03:05 +0100

    commit inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello World!</h1>
+      <div className="card">
+      </div>
+      
+    </>
+  )
+}
+
+export default App
