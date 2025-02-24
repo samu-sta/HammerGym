a3b5fb0f37031e2958416a3d11688e2c1219a715
TheGamas <samuelfelipelorente@gmail.com>
2025-02-24 11:48:07 +0100

    añadido script init-db

--- backend/src/database/initDB.js
+++ backend/src/database/initDB.js
@@ -1,8 +1,26 @@
 import sequelize from './database.js';
+import UserModel from '../models/User.js';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const initDatabase = async () => {
   try {
+
+    const tempSequelize = new Sequelize({
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: 'localhost',
+      dialect: 'mysql'
+    });
+
+    await tempSequelize.query('CREATE DATABASE IF NOT EXISTS `hammergym`');
+    await tempSequelize.close();
+
+    await UserModel.sync();
     await sequelize.sync({ force: false });
+
     console.log('Database & tables created!');
   } catch (error) {
     console.error('Error creating database:', error);
