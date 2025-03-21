31287015c7c9fecea6c25c7c51595dff8d6a08d7
Samuel <samuelfelipelorente@gmail.com>
2025-03-21 19:40:59 +0100

    añadidas relaciones de Account con User, Trainer y Admin

--- backend/src/database/associations.js
+++ backend/src/database/associations.js
@@ -1,8 +1,47 @@
 import UserModel from '../models/User.js';
+import AccountModel from '../models/Account.js';
+import TrainerModel from '../models/Trainer.js';
+import AdminModel from '../models/Admin.js';
 import GymModel from '../models/Gym.js';
 import UserActivityModel from '../models/UserActivity.js';
 
 const setupAssociations = () => {
+
+  /* Account Associations */
+
+  // Account - User (One-to-One)
+  AccountModel.hasOne(UserModel, {
+    foreignKey: 'accountId',
+    as: 'user'
+  });
+
+  UserModel.belongsTo(AccountModel, {
+    foreignKey: 'accountId',
+    as: 'account'
+  });
+
+  // Account - Trainer (One-to-One)
+  AccountModel.hasOne(TrainerModel, {
+    foreignKey: 'accountId',
+    as: 'trainer'
+  });
+
+  TrainerModel.belongsTo(AccountModel, {
+    foreignKey: 'accountId',
+    as: 'account'
+  });
+
+  // Account - Admin (One-to-One)
+  AccountModel.hasOne(AdminModel, {
+    foreignKey: 'accountId',
+    as: 'admin'
+  });
+
+  AdminModel.belongsTo(AccountModel, {
+    foreignKey: 'accountId',
+    as: 'account'
+  });
+
   GymModel.hasMany(UserActivityModel, {
     foreignKey: 'gymId',
     as: 'activities'
